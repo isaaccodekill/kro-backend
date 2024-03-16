3c39764beae9250d44342f8754b10ef7a28c086e
isaacbello <isaac@teachino.io>
2024-03-16 14:09:07 +0100

    create Transaction response type

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from app.enums import TransactionType
+
+from datetime import datetime
+
 
 class Transaction(BaseModel):
     id: int
     amount: float
+    type: TransactionType = Field(default=TransactionType.DEPOSIT)
+    timestamp: datetime
+    payment_method: str = None
+    user_id: int
