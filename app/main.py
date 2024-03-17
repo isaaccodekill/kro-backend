7abcf9d98fa1ca9a974893ee7cf1cf1c9821f975
isaacbello <isaac@teachino.io>
2024-03-17 14:21:46 +0100

    handle 500 errors gracefullyy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import os.path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from dotenv import load_dotenv
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 print("on_startup")
 dotenv_path = os.path.join('..', '.env')
@@ -34,6 +36,23 @@ app.add_middleware(
 @app.on_event("startup")
 async def on_startup():
     create_tables()
+
+
+# Generic exception handler for HTTPException (internal server errors)
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+
+
+# Custom exception handler for internal server errors (generic)
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
+
+
+@app.exception_handler(500)
+async def internal_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(status_code=500, content=jsonable_encoder({"detail": "Internal Server Error"}))
 
 
 app.include_router(user.router, prefix="/api/v1/user")
